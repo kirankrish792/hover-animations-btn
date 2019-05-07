bf9682267c100860cfc6d786dc75e93d39d48219
javiluli <javidel96@hotmail.es>
2019-05-07 15:29:52 +0200

    Create and finished butonn -> E-15

--- public/JS/scripts.js
+++ public/JS/scripts.js
@@ -1,12 +1,22 @@
 // Copyright for new (Date().getFullYear())
 
-  let newDateYear= (new Date().getFullYear());
-  let copyrightText = document.getElementById('textFooter');
+let newDateYear = new Date().getFullYear();
+let copyrightText = document.getElementById("textFooter");
 
-  copyrightText.innerHTML = 'Copyright &copy; 1996-' + newDateYear + ' Javier Delgado |';
+copyrightText.innerHTML =
+  "Copyright &copy; 1996-" + newDateYear + " Javier Delgado |";
 //---------------------------------
 
 // Test message connected with HTML
-let greet = 'Hellor world!!';
+let greet = "Hellor world!!";
 console.log(greet.toUpperCase());
+//---------------------------------
+
+// Function Jquery for Btn-E15
+$("#button-btn-E15").click(function() {
+  $(this).toggleClass("clicked");
+  $("#button-btn-E15 #p-btn-E15").text(function(i, text) {
+    return text === "Sent!" ? "Send" : "Sent!";
+  });
+});
 //---------------------------------
