20fbca0e7291c0cf406894005fe1a719d44f3107
javiluli <javidel96@hotmail.es>
2019-05-08 13:46:53 +0200

    Create and finished butonn -> B-17

--- public/JS/scripts.js
+++ public/JS/scripts.js
@@ -39,3 +39,21 @@ for (var i = 0; i < bubblyButtons.length; i++) {
   bubblyButtons[i].addEventListener("click", animateButton, false);
 }
 //---------------------------------
+
+// Function Jquery for Btn-B17
+$(window).ready(function() {
+  $(".btn-B17").wrapInner("<div class=botontext></div>");
+
+  $(".botontext")
+    .clone()
+    .appendTo($(".btn-B17"));
+
+  $(".btn-B17").append(
+    '<span class="twist"></span><span class="twist"></span><span class="twist"></span><span class="twist"></span>'
+  );
+
+  $(".twist")
+    .css("width", "25%")
+    .css("width", "+=3px");
+});
+//---------------------------------
